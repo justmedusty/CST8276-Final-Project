4f5365dc9b32ddd5a8b365fc4bb215ad37450e7f
justmedusty <dustyn_gibb@hotmail.com>
2023-02-06 06:44:43 -0500

    Test Commit

--- weather-scraper/main.py
+++ weather-scraper/main.py
@@ -1,11 +1,8 @@
 import asyncio
-#making sure this comment pushes
+# making sure this comment pushes
 from scrape import scrape_station_details, scrape_weather_stations
 
 stations = asyncio.run(scrape_station_details())
 
 while True:
     asyncio.run(scrape_weather_stations(stations))
-
-
-
